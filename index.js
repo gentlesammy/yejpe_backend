464127b5d5993c85ce0312519c59ac874911c252
Gentlesammy <samodunlade84@gmail.com>
2021-04-06 14:12:07 +0100

    deployment

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ const io = require("socket.io")(server, {
         origin: "*",
         methods: ["GET", "POST"], 
     },
-
 })
 app.use(cors()); 
 const PORT = process.env.PORT || 8000;
 app.get("/", (req, res)=> {
-    res.send("<h1>HELLO WORLD</h1>");
+    res.send("<h1>Yejide Pe video caller App</h1>");
 })
+
+io.on("connection",   (socket)=> {
+    //initial when connected
+    socket.emit("me", socket.id);
+    //call ended
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callended");
+    })
+    //user is called, data will be passed from frontend
+    socket.on("calluser", ({userToCall, signalData, from, name})=> {
+        io.to(userToCall).emit("calluser", {signal:signalData, from, name})
+    })
+
+    //call is answered
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    })
+
+})
+
 server.listen(PORT, () => {
   console.log(`connecting on port ${PORT}`);
 });
